afcdd81e8bf588135ae5a7169907650466d57c21
희주 <149589749+heeju97@users.noreply.github.com>
2024-01-08 16:55:15 +0900

    js
    js

---
+++ app2.js
@@ -0,0 +1,52 @@
+// 데이터타입
+// 1. Number
+console.log(name1);
+console.log(typeof name1);
+
+
+// 2. String
+console.log('heeju');
+let name1 = 'juju';
+console.log(name1);
+
+console.log(name1);
+console.log(typeof name1);
+
+
+// template literal
+let myName = 'heeju';
+let templateLiteral = `hello${name1}`
+console.log(templateLiteral);
+
+
+// 3. Boolean
+console.log(true);
+let isFun = false;
+console.log(isFun);
+console.log(typeof isFun);
+
+
+// 4. Undefined (정의되지 않음)
+let apple;
+console.log(apple);
+console.log(typeof apple);
+
+
+// 5. Null
+let time = null;
+console.log(time);
+console.log(typeof time)
+
+
+// 6. Symbol(ES6)
+let symbol = Symbol();
+console.log(symbol);
+console.log(typeof symbol);
+
+
+// 7. BigInt
+let theBiggestInt = 9876543n
+console.log(theBiggestInt);
+console.log(123123);
+console.log(typeof 123123);
+console.log(typeof theBiggestInt);
